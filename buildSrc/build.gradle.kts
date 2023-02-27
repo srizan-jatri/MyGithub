fa1cae44460a0b0aecb014857056c6011ebac1aa
Srizan <srizan.jatri@gmail.com>
2023-02-27 11:19:48 +0600

    added firebase_app_distribution using gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,6 +15,12 @@ dependencies {
     implementation("com.android.tools.build:gradle:7.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.44")
+
+    // Make sure that you have the Google services Gradle plugin dependency
+    implementation("com.google.gms:google-services:4.3.15")
+
+    // Add the dependency for the App Distribution Gradle plugin
+    implementation("com.google.firebase:firebase-appdistribution-gradle:3.2.0")
 }
 
 gradlePlugin {
